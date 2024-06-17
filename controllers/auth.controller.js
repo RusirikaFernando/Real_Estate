1cc91f7a1ea32610e4be845803d83b0c8dadcb9f
RusirikaFernando <thathsaranif7@gmail.com>
2024-06-17 23:05:58 +0530

    Add JWT authentication for login and logout
    - Implemented JWT token generation for user login
    - Set JWT token as an HTTP-only cookie with a 7-day expiration
    - Added error handling for login process
    - Implemented logout functionality to clear the JWT token cookie

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
@@ -42,6 +43,19 @@ export const login = async (req, res) => {
     if(!isPasswordValid) return res.status(401).json({message:"Invalid credentials!"});
     
     //generate cookie token and send to the user
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+    const age = 1000*60*60*24*7;
+
+const token =jwt.sign({
+    id:user.id
+}, process.env.JWT_SECRET_KEY,{expiresIn:age})
+
+    res.cookie("token",token,{
+        httpOnly:true,
+        //secure:ture
+        maxAge:age,
+
+    }).status(200).json({message:"Login Successful"})
 
 }catch(err){
     console.log(err)
@@ -49,4 +63,7 @@ export const login = async (req, res) => {
 }
 };
 
-export const logout = (req, res) => {};
+
+export const logout = (req, res) => {
+    res.clearCookie("token").status(200).json({message:"Logout Successful"});
+};
