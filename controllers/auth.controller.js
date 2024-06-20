bd92951f0f96b9e827eeeb2886831defb8d7d0bc
RusirikaFernando <thathsaranif7@gmail.com>
2024-06-21 00:12:12 +0530

    Add authentication and authorization routes and controllers
    - Created testroute.js with routes for authentication and authorization checks.
    - Implemented shouldBeLoggedIn and shouldBeAdmin controllers in test.controller.js.
    - Added JWT token verification and role-based access control in the controllers.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -29,41 +29,49 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-    const{ username, password} = req.body;
+  const { username, password } = req.body;
 
-    try{
+  try {
     //check if the user exists
     const user = await prisma.user.findUnique({
-        where:{username}
+      where: { username },
     });
- if(!user) return res.status(401).json({message:"Invalid credentials!"});
+    if (!user) return res.status(401).json({ message: "Invalid credentials!" });
     //check if the password is correct
 
-    const isPasswordValid =await bcrypt.compare(password, user.password);
-    if(!isPasswordValid) return res.status(401).json({message:"Invalid credentials!"});
-    
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid credentials!" });
+
     //generate cookie token and send to the user
     // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
-    const age = 1000*60*60*24*7;
+    const age = 1000 * 60 * 60 * 24 * 7;
 
-const token =jwt.sign({
-    id:user.id
-}, process.env.JWT_SECRET_KEY,{expiresIn:age})
+    const token = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: false,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
 
-    res.cookie("token",token,{
-        httpOnly:true,
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
         //secure:ture
-        maxAge:age,
-
-    }).status(200).json({message:"Login Successful"})
-
-}catch(err){
-    console.log(err)
-    res.status(500).json({message:"Failed to Login"})
-}
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to Login" });
+  }
 };
 
-
 export const logout = (req, res) => {
-    res.clearCookie("token").status(200).json({message:"Logout Successful"});
+  res.clearCookie("token").status(200).json({ message: "Logout Successful" });
 };
