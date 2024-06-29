76520c874b9d7d1f2ac229b717dd100f929b2140
RusirikaFernando <thathsaranif7@gmail.com>
2024-06-29 10:25:54 +0530

    refactor findMany function

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,9 +1,22 @@
 import prisma from "../lib/prisma.js";
- 
+
 export const getPosts = async (req, res) => {
+  const query = req.query;
+
   try {
-    const posts = await prisma.post.findMany();
- 
+    const posts = await prisma.post.findMany({
+      where: {
+        city: query.city || undefined,
+        type: query.type || undefined,
+        property: query.property || undefined,
+        bedroom: parseInt(query.bedroom) || undefined,
+        price: {
+          gte: parseInt(query.minPrice) || 0,
+          lte: parseInt(query.maxPrice) || 1000000,
+        },
+      },
+    });
+
     res.status(200).json(posts);
   } catch (err) {
     console.log(err);
@@ -15,17 +28,17 @@ export const getPost = async (req, res) => {
   try {
     const post = await prisma.post.findUnique({
       where: { id },
-      include:{
-        postDetail:true,
-        user:{
-            select:{
-                username:true,
-                avatar:true
-            }
-        }
-      }
+      include: {
+        postDetail: true,
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+      },
     });
- 
+
     res.status(200).json(post);
   } catch (err) {
     console.log(err);
@@ -35,7 +48,7 @@ export const getPost = async (req, res) => {
 export const addPost = async (req, res) => {
   const body = req.body;
   const tokenUserId = req.userId;
- 
+
   try {
     const newPost = await prisma.post.create({
       data: {
@@ -67,11 +80,11 @@ export const deletePost = async (req, res) => {
     const post = await prisma.post.findUnique({
       where: { id },
     });
- 
+
     if (post.userId !== tokenUserId) {
       return res.status(403).json({ message: "not authorized" });
     }
- 
+
     await prisma.post.delete({
       where: { id },
     });
@@ -81,4 +94,3 @@ export const deletePost = async (req, res) => {
     res.status(500).json({ message: "failed to delete post" });
   }
 };
- 
