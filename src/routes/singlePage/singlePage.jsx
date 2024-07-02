38bd6d3b45999f6231809b6b5e0b42ab76027d2f
RusirikaFernando <thathsaranif7@gmail.com>
2024-07-02 13:33:43 +0530

    feat: Add optimistic save functionality
    - Toggle saved state immediately for better UX
    - Check user authentication before saving
    - Redirect to login if not authenticated
    - Handle API request to save/unsave post

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,13 +1,32 @@
 import "./singlePage.scss";
 import Slider from "../../components/slider/Slider";
 import Map from "../../components/map/Map";
-import { singlePostData, userData } from "../../lib/dummydata";
-import { useLoaderData } from "react-router-dom";
+import { redirect, useLoaderData } from "react-router-dom";
 import DOMPurify from "dompurify";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
 
 function SinglePage() {
   const post = useLoaderData();
   console.log(post);
+  const { currentUser } = useContext(AuthContext);
+  const [saved, setSaved] = useState(post.isSaved);
+
+  const handleSave = async () => {
+    // after react 19 update to useoptimistik hook
+
+    setSaved((prev) => !prev);
+    if (!currentUser) {
+      redirect("/login");
+    }
+    try {
+      await apiRequest.post("/users/save", { postId: post.id });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="singlePage">
       <div className="details">
@@ -124,9 +143,12 @@ function SinglePage() {
               <img src="/chat.png" alt="" />
               Send a Message
             </button>
-            <button>
+            <button
+              onClick={handleSave}
+              style={{ backgroundColor: saved ? "#fece51" : "white" }}
+            >
               <img src="/save.png" alt="" />
-              Save the Place
+              {saved ? "Place Saved" : "place is not saved"}
             </button>
           </div>
         </div>
