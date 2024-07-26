aa8d4e0235d416f435292dc4b0a6b9c85901fa1a
RusirikaFernando <thathsaranif7@gmail.com>
2024-07-26 13:31:58 +0530

    add a useeffect to track the changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,24 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import { useNotificationStore } from "../../lib/notificationStore";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
 
-  const {currentUser} = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
+  const fetch = useNotificationStore((state) => state.fetch);
+  const number = useNotificationStore((state) => state.number);
+
+ 
+  useEffect(() => {
+    if (currentUser) {
+      fetch();
+    }
+  }, [currentUser]);
+//  console.log(currentUser)
   // const user = true;
   return (
     <nav>
@@ -24,13 +35,10 @@ function Navbar() {
       <div className="right">
         {currentUser ? (
           <div className="user">
-            <img
-              src={currentUser.avatar  || "/noavatar.jpg"}
-              alt=""
-            />
+            <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
             <span>{currentUser.username}</span>
             <Link to="/profile" className="profile">
-              <div className="notification">3</div>
+              {number > 0 && <div className="notification">{number}</div>}
               <span>Profile</span>
             </Link>
           </div>
